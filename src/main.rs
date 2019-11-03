baaee441a4f6261d7e49a8ce0d54b0c970e75101
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2019-11-03 13:10:43 +0800

    Support fzy using rff

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,56 @@
-use std::env;
 use std::io::{self, BufRead};
-use std::process::exit;
 
 use fuzzy_matcher::skim::fuzzy_indices;
-use indexmap::IndexMap;
+use rayon::prelude::*;
+use rff::match_and_score_with_positions;
 use serde_json::json;
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "maple")]
+struct Opt {
+    /// Initial query string
+    #[structopt(index = 1, short, long)]
+    query: String,
+
+    /// Filter algorithm
+    #[structopt(short, long, default_value = "fzy")]
+    algo: String,
+}
+
+#[inline]
+pub fn scorer(algo: &str, query: &str, line: &str) -> Option<(f64, Vec<usize>)> {
+    match algo.into() {
+        "skim" => fuzzy_indices(line, query).map(|(score, indices)| (score as f64, indices)),
+        "fzy" => {
+            match_and_score_with_positions(query, line).map(|(_, score, indices)| (score, indices))
+        }
+        _ => unreachable!(),
+    }
+}
 
 pub fn main() {
-    let args: Vec<String> = env::args().collect();
+    let opt = Opt::from_args();
 
-    if args.len() != 2 {
-        println!("Usage: echo <piped_input> | maple <pattern>");
-        exit(1);
-    }
+    let query = &*opt.query;
+    let algo = &*opt.algo;
 
-    let query = &args[1];
-
-    let mut ranked = IndexMap::new();
+    let mut ranked = vec![];
 
     for line in io::stdin().lock().lines() {
         if let Ok(line) = line {
-            if let Some((score, indices)) = fuzzy_indices(&line, query) {
-                ranked.insert(line, (score, indices));
+            if let Some((score, indices)) = scorer(algo, query, &line) {
+                ranked.push((line, score, indices));
             }
         }
     }
 
-    ranked.sort_by(|_, v1, _, v2| v2.0.cmp(&v1.0));
+    ranked.par_sort_by(|(_, v1, _), (_, v2, _)| v2.partial_cmp(&v1).unwrap());
 
-    for (k, v) in ranked.iter() {
+    for (text, _, indices) in ranked.iter() {
         let matched = json!({
-            "text": k,
-            "indices": v.1,
+          "text": text,
+          "indices": indices,
         });
         println!("{}", matched.to_string());
     }
