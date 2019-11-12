5958f90ce8048898cd42c4bbfd9d782f771a499c
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2019-11-12 12:16:04 +0800

    Refine maple

--- src/main.rs
+++ src/main.rs
@@ -18,40 +18,40 @@ struct Opt {
     algo: String,
 }
 
-#[inline]
-pub fn scorer(algo: &str, query: &str, line: &str) -> Option<(f64, Vec<usize>)> {
-    match algo.into() {
-        "skim" => fuzzy_indices(line, query).map(|(score, indices)| (score as f64, indices)),
-        "fzy" => {
-            match_and_score_with_positions(query, line).map(|(_, score, indices)| (score, indices))
-        }
-        _ => unreachable!(),
-    }
-}
-
 pub fn main() {
     let opt = Opt::from_args();
 
     let query = &*opt.query;
     let algo = &*opt.algo;
 
-    let mut ranked = vec![];
-
-    for line in io::stdin().lock().lines() {
-        if let Ok(line) = line {
-            if let Some((score, indices)) = scorer(algo, query, &line) {
-                ranked.push((line, score, indices));
-            }
+    let scorer = |line: &str| match algo.into() {
+        "skim" => fuzzy_indices(line, query).map(|(score, indices)| (score as f64, indices)),
+        "fzy" => {
+            match_and_score_with_positions(query, line).map(|(_, score, indices)| (score, indices))
         }
-    }
+        _ => unreachable!(),
+    };
 
-    ranked.par_sort_by(|(_, v1, _), (_, v2, _)| v2.partial_cmp(&v1).unwrap());
+    // Result<Option<T>> => T
+    let mut ranked = io::stdin()
+        .lock()
+        .lines()
+        .filter_map(|lines_iter| {
+            lines_iter
+                .ok()
+                .and_then(|line| scorer(&line).map(|(score, indices)| (line, score, indices)))
+        })
+        .collect::<Vec<_>>();
+
+    ranked.par_sort_unstable_by(|(_, v1, _), (_, v2, _)| v2.partial_cmp(&v1).unwrap());
 
     for (text, _, indices) in ranked.iter() {
-        let matched = json!({
-          "text": text,
-          "indices": indices,
-        });
-        println!("{}", matched.to_string());
+        println!(
+            "{}",
+            json!({
+            "text": text,
+            "indices": indices,
+            })
+        );
     }
 }
