997f60aedb93c0b4cfa09cc96c97e86fc8a32b4c
ImmConCon <43708554+ImmemorConsultrixContrarie@users.noreply.github.com>
2019-12-26 18:00:02 +0200

    Move fzy algo from rff crate into local file and some other small tweaks to Rust code (#180)
    * Less allocations, more laziness
    
    If `find_start_at()` returns `None`, the function instantly returns `None` too. So instead of lowercasing the whole string and then splitting it into words, split it and then lowercase given words. Less lowercasing if `find_start_at()` will exit early.
    
    Also removed vector of indices. Those could be created in place, so no need for allocation of the whole vector. Besides, `vec.extend_from_slice(slice)` desugars into something like this:
    
    for element in slice.iter().cloned() {
        vec.push(element);
    }
    
    * Extracted fzy algorithm from rff crate
    
    The biggest change is fzy algo being moved from "rff" crate into one of the files in the directory.
    Some very little changes done to the algorithm while moving.
    Now Rust code could be checked and tested not on unix systems only.
    
    Also added ".lock" to gitignore for libs.
    The reason is explained here:
    https://doc.rust-lang.org/cargo/faq.html#why-do-binaries-have-cargolock-in-version-control-but-not-libraries
    
    * Do not .gitignore "Cargo.lock" files
    
    Removed *.lock from .gitignore for libs, added my (Windows) .locks to the pushed files.

--- pythonx/clap/fuzzymatch-rs/src/lib.rs
+++ pythonx/clap/fuzzymatch-rs/src/lib.rs
@@ -1,8 +1,8 @@
 #![feature(pattern)]
 
+use extracted_fzy::match_and_score_with_positions;
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
-use rff::match_and_score_with_positions;
 
 use std::str::pattern::Pattern;
 
@@ -12,20 +12,29 @@ fn find_start_at<'a, P: Pattern<'a>>(slice: &'a str, at: usize, pat: P) -> Optio
 }
 
 fn substr_scorer(niddle: &str, haystack: &str) -> Option<(f64, Vec<usize>)> {
-    let niddle = niddle.to_lowercase();
     let haystack = haystack.to_lowercase();
-    let indices: Vec<usize> = (0..haystack.len()).collect();
     let haystack = haystack.as_str();
 
     let mut offset = 0;
     let mut positions = Vec::new();
     for sub_niddle in niddle.split_whitespace() {
-        match find_start_at(haystack, offset, sub_niddle) {
+        let sub_niddle = sub_niddle.to_lowercase();
+
+        match find_start_at(haystack, offset, &sub_niddle) {
             Some(idx) => {
-                offset = idx;
-                let niddle_len = sub_niddle.len();
-                positions.extend_from_slice(&indices[offset..offset + niddle_len]);
-                offset += niddle_len;
+                offset = idx + sub_niddle.len();
+                // For build without overflow checks this could be written as
+                // `let mut pos = idx - 1;` with `|| { pos += 1; pos }` closure.
+                let mut pos = idx;
+                positions.resize_with(
+                    positions.len() + sub_niddle.len(),
+                    // Simple endless iterator for `idx..` range. Even though it's endless,
+                    // it will iterate only `sub_niddle.len()` times.
+                    || {
+                        pos += 1;
+                        pos - 1
+                    },
+                );
             }
             None => return None,
         }
@@ -50,9 +59,7 @@ fn fuzzy_match(query: &str, candidates: Vec<String>) -> PyResult<(Vec<Vec<usize>
     let scorer: Box<dyn Fn(&str) -> Option<(f64, Vec<usize>)>> = if query.contains(" ") {
         Box::new(|line: &str| substr_scorer(query, line))
     } else {
-        Box::new(|line: &str| {
-            match_and_score_with_positions(query, line).map(|(_, score, indices)| (score, indices))
-        })
+        Box::new(|line: &str| match_and_score_with_positions(query, line))
     };
 
     let mut ranked = candidates
