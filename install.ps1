0207e97827311e6fa0c87029952eb22372e0759e
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2020-01-15 16:14:07 +0800

    0.5

--- install.ps1
+++ install.ps1
@@ -1,6 +1,6 @@
 #!/usr/bin/env pwsh
 
-$version = 'v0.4'
+$version = 'v0.5'
 $APP = 'maple'
 $url = "https://github.com/liuchengxu/vim-clap/releases/download/$version/$APP-$version-"
 $output = "$PSScriptRoot\bin\$APP.exe"
