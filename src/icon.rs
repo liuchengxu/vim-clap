6106b62b8dc294c8ba59643849c4e6019fff3255
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2020-02-24 17:06:27 +0800

    .

--- src/icon.rs
+++ src/icon.rs
@@ -180,7 +180,7 @@ pub fn icon_for_path(path: &Path) -> &str {
         })
 }
 
-pub fn icon_for(line: &str) -> &str {
+fn icon_for(line: &str) -> &str {
     let path = Path::new(line);
     icon_for_path(&path)
 }
