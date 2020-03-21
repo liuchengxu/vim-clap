e63e4ebdb47ec676f6de037e460af5fb186fdcd0
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2020-03-21 12:41:42 +0800

    Nits

--- crates/icon/update_constants.py
+++ crates/icon/update_constants.py
@@ -1,24 +1,24 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+#  Whenever you change exactmatch_map.json or extension_map.json,
+#  rerun this script to regenerate src/constants.rs:
+#    ./update_constants.py
 import json
 
 
+#  Read exactmatch_map.json and extension_map.json into the form of
+#  ("ai", ""),("awk", ""),("bash", ""), ordered by the key.
 def into_joined_tuples(icon_json_source):
     with open(icon_json_source, 'r') as f:
         disordered = json.load(f)
         sorted_dict = {k: disordered[k] for k in sorted(disordered)}
-
-        with open('sorted_extension_map.json', 'w') as fp:
-            json.dump(sorted_dict, fp, indent=2)
 
         joined_tuples = ','.join(
             map(lambda kv: '("%s", "%s")' % (kv[0], kv[1]),
                 sorted_dict.items()))
         return joined_tuples
 
-
-joined_tuples = into_joined_tuples('exactmatch_map.json')
 
 with open('src/constants.rs', 'w') as f:
     decl_exactmatch_map = ("pub static ref EXACTMATCH_MAP: "
