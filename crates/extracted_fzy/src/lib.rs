97aefe75ba6eb4c01d0cfdb50e45d74829374679
ImmConCon <somewhat.fluffy@gmail.com>
2020-03-23 08:57:33 +0200

    Really small change in doc-test from `ignored` to `compile_fail`.

--- crates/extracted_fzy/src/lib.rs
+++ crates/extracted_fzy/src/lib.rs
@@ -36,7 +36,8 @@ pub fn match_and_score_with_positions<'a>(
 ///
 /// # Examples
 ///
-/// ```ignore
+// This is a proper code, that should compile, but `matches()` function is private.
+/// ```compile_fail
 /// assert_eq!(Some(5), extracted_fzy::matches("amo汉漢", "app/models/order/汉语/漢語"));
 /// assert_eq!(6, "汉漢".len()); // Length of this two chars in bytes.
 /// ```
