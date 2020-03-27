b9316cda8e40a0171127615f201eeb64290f8312
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2020-03-27 12:29:19 +0800

    .

--- crates/icon/src/lib.rs
+++ crates/icon/src/lib.rs
@@ -21,7 +21,7 @@ type Icon = char;
 ///
 /// Try matching the exactmatch map against the file name, and then the extension map.
 #[inline]
-pub fn get_icon_char(path: &Path, default: char) -> char {
+pub fn get_icon_or(path: &Path, default: char) -> char {
     path.file_name()
         .and_then(std::ffi::OsStr::to_str)
         .and_then(|filename| {
@@ -41,7 +41,7 @@ pub fn get_icon_char(path: &Path, default: char) -> char {
 
 fn icon_for(line: &str) -> Icon {
     let path = Path::new(line);
-    get_icon_char(&path, DEFAULT_ICON)
+    get_icon_or(&path, DEFAULT_ICON)
 }
 
 pub fn prepend_icon(line: &str) -> String {
@@ -53,7 +53,7 @@ pub fn icon_for_filer(path: &Path) -> Icon {
     if path.is_dir() {
         FOLDER_ICON
     } else {
-        get_icon_char(path, DEFAULT_FILER_ICON)
+        get_icon_or(path, DEFAULT_FILER_ICON)
     }
 }
 
@@ -71,14 +71,4 @@ pub fn prepend_grep_icon(line: &str) -> String {
         .map(|m| icon_for(m.as_str()))
         .unwrap_or(DEFAULT_ICON);
     format!("{} {}", icon, line)
-}
-
-#[test]
-fn test_table() {
-    static EXTENSION_TABLE: &[(&[u8], char)] = &[(b"ai", ''), (b"awk", ''), (b"bash", '')];
-    fn bsearch_case_table(c: &[u8], table: &[(&[u8], char)]) -> Option<usize> {
-        table.binary_search_by(|&(key, _)| key.cmp(&c)).ok()
-    }
-    println!("{:?}", bsearch_case_table(b"ai", EXTENSION_TABLE));
-    println!("{:?}", bsearch_case_table(b"bash", EXTENSION_TABLE));
 }
