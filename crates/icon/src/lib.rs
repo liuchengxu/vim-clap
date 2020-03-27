7e7da17a28974a2a35de3009395b419f9654e008
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2020-03-27 11:52:52 +0800

    Use static reference to the const icon table

--- crates/icon/src/lib.rs
+++ crates/icon/src/lib.rs
@@ -1,50 +1,54 @@
 mod constants;
 
-use constants::{EXACTMATCH, EXACTMATCH_ICON, EXTENSION, EXTENSION_ICON};
+use constants::{bsearch_icon_table, EXACTMATCH_ICON_TABLE, EXTENSION_ICON_TABLE};
 
 use std::path::Path;
 
 use lazy_static::lazy_static;
 use regex::Regex;
 
-pub const DEFAULT_ICON: &str = "";
+// pub const DEFAULT_ICON: &str = "";
+pub const DEFAULT_ICON: char = '';
+pub const FOLDER_ICON: char = '';
+pub const DEFAULT_FILER_ICON: char = '';
+
 pub const DEFAULT_ICONIZED: &str = " ";
-pub const FOLDER_ICON: &str = "";
-pub const DEFAULT_FILER_ICON: &str = "";
 
 /// The type used to represent icons.
 ///
 /// This could be changed into different type later,
 /// so functions take and return this type, not `char` or `&str` directly.
-type Icon = &'static str;
+type Icon = char;
 
 /// Return appropriate icon for the path. If no icon matched, return the specified default one.
 ///
 /// Try matching the exactmatch map against the file name, and then the extension map.
 #[inline]
-pub fn get_icon_or(path: &Path, default: Icon) -> Icon {
+pub fn get_icon_char(path: &Path, default: char) -> char {
     path.file_name()
         .and_then(std::ffi::OsStr::to_str)
-        .and_then(
-            |filename| match EXACTMATCH.binary_search(&filename.to_lowercase().as_str()) {
-                Ok(idx) => Some(EXACTMATCH_ICON[idx]),
-                Err(_) => None,
-            },
-        )
+        .and_then(|filename| {
+            match bsearch_icon_table(filename.to_lowercase().as_bytes(), EXACTMATCH_ICON_TABLE) {
+                Some(idx) => Some(EXACTMATCH_ICON_TABLE[idx].1),
+                None => None,
+            }
+        })
         .unwrap_or_else(|| {
             path.extension()
                 .and_then(std::ffi::OsStr::to_str)
-                .and_then(|ext| match EXTENSION.binary_search(&ext) {
-                    Ok(idx) => Some(EXTENSION_ICON[idx]),
-                    Err(_) => None,
-                })
-                .unwrap_or(&default)
+                .and_then(
+                    |ext| match bsearch_icon_table(ext.as_bytes(), EXTENSION_ICON_TABLE) {
+                        Some(idx) => Some(EXTENSION_ICON_TABLE[idx].1),
+                        None => None,
+                    },
+                )
+                .unwrap_or(default)
         })
 }
 
 fn icon_for(line: &str) -> Icon {
     let path = Path::new(line);
-    get_icon_or(&path, DEFAULT_ICON)
+    get_icon_char(&path, DEFAULT_ICON)
 }
 
 pub fn prepend_icon(line: &str) -> String {
@@ -56,7 +60,7 @@ pub fn icon_for_filer(path: &Path) -> Icon {
     if path.is_dir() {
         FOLDER_ICON
     } else {
-        get_icon_or(path, DEFAULT_FILER_ICON)
+        get_icon_char(path, DEFAULT_FILER_ICON)
     }
 }
 
@@ -74,4 +78,14 @@ pub fn prepend_grep_icon(line: &str) -> String {
         .map(|m| icon_for(m.as_str()))
         .unwrap_or(DEFAULT_ICON);
     format!("{} {}", icon, line)
+}
+
+#[test]
+fn test_table() {
+    static EXTENSION_TABLE: &[(&[u8], char)] = &[(b"ai", ''), (b"awk", ''), (b"bash", '')];
+    fn bsearch_case_table(c: &[u8], table: &[(&[u8], char)]) -> Option<usize> {
+        table.binary_search_by(|&(key, _)| key.cmp(&c)).ok()
+    }
+    println!("{:?}", bsearch_case_table(b"ai", EXTENSION_TABLE));
+    println!("{:?}", bsearch_case_table(b"bash", EXTENSION_TABLE));
 }
