e6c28fcf3df9eab29b531955929f2a1b7de2673a
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2020-03-30 10:53:23 +0800

    Split out print_top_items()

--- crates/maple_cli/src/cmd/filter/mod.rs
+++ crates/maple_cli/src/cmd/filter/mod.rs
@@ -7,6 +7,35 @@ use fuzzy_filter::{fuzzy_filter_and_rank, truncate_long_matched_lines, Algo, Sou
 
 use icon::prepend_icon;
 
+/// Return the info of the truncated top items ranked by the filtering score.
+fn print_top_items<T>(
+    total: usize,
+    top_size: usize,
+    top_list: impl IntoIterator<Item = (String, T, Vec<usize>)>,
+    winwidth: usize,
+    enable_icon: bool,
+) {
+    let (truncated_lines, truncated_map) = truncate_long_matched_lines(top_list, winwidth, None);
+    let mut lines = Vec::with_capacity(top_size);
+    let mut indices = Vec::with_capacity(top_size);
+    if enable_icon {
+        for (text, _, idxs) in truncated_lines {
+            lines.push(prepend_icon(&text));
+            indices.push(idxs);
+        }
+    } else {
+        for (text, _, idxs) in truncated_lines {
+            lines.push(text);
+            indices.push(idxs);
+        }
+    }
+    if truncated_map.is_empty() {
+        println_json!(total, lines, indices);
+    } else {
+        println_json!(total, lines, indices, truncated_map);
+    }
+}
+
 pub fn run<I: Iterator<Item = String>>(
     query: &str,
     source: Source<I>,
@@ -19,29 +48,13 @@ pub fn run<I: Iterator<Item = String>>(
 
     if let Some(number) = number {
         let total = ranked.len();
-        let payload = ranked.into_iter().take(number);
-        let winwidth = winwidth.unwrap_or(62);
-        let (truncated_payload, truncated_map) =
-            truncate_long_matched_lines(payload, winwidth, None);
-        let mut lines = Vec::with_capacity(number);
-        let mut indices = Vec::with_capacity(number);
-        if enable_icon {
-            for (text, _, idxs) in truncated_payload {
-                let iconized = prepend_icon(&text);
-                lines.push(iconized);
-                indices.push(idxs);
-            }
-        } else {
-            for (text, _, idxs) in truncated_payload {
-                lines.push(text);
-                indices.push(idxs);
-            }
-        }
-        if truncated_map.is_empty() {
-            println_json!(total, lines, indices);
-        } else {
-            println_json!(total, lines, indices, truncated_map);
-        }
+        print_top_items(
+            total,
+            number,
+            ranked.into_iter().take(number),
+            winwidth.unwrap_or(62),
+            enable_icon,
+        );
     } else {
         for (text, _, indices) in ranked.iter() {
             println_json!(text, indices);
