7f621f072fe161a62c5dc04ee0680643eea34065
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2020-04-07 09:22:38 +0800

    Update cache subcommand

--- crates/maple_cli/src/cmd/cache.rs
+++ crates/maple_cli/src/cmd/cache.rs
@@ -2,25 +2,38 @@ use crate::utils::clap_cache_dir;
 use anyhow::Result;
 use std::fs::read_dir;
 
-pub fn run(list: bool) -> Result<()> {
+#[cfg(target_os = "windows")]
+const PATH_SEPERATOR: &str = r"\";
+
+#[cfg(not(target_os = "windows"))]
+const PATH_SEPERATOR: &str = "/";
+
+pub fn run(list_entries: bool) -> Result<()> {
     let cache_dir = clap_cache_dir();
-    println!("Current cache directory: {:?}", cache_dir);
-    if list {
+    let cache_dir_str = cache_dir.clone().into_os_string().into_string().unwrap();
+    println!("Current cache directory:");
+    println!("\t{}", cache_dir_str);
+    if list_entries {
+        println!("Cached entries:");
         let mut entries = read_dir(cache_dir)?
             .map(|res| {
                 res.map(|e| {
-                    e.path()
+                    let fname: String = e
+                        .path()
                         .file_name()
                         .and_then(std::ffi::OsStr::to_str)
                         .map(Into::into)
-                        .unwrap()
+                        .expect("Couldn't get file name");
+                    format!("{}{}{}", cache_dir_str, PATH_SEPERATOR, fname)
                 })
             })
             .collect::<Result<Vec<String>, std::io::Error>>()?;
 
         entries.sort();
 
-        println!("Cached entries: {:?}", entries);
+        for entry in entries {
+            println!("\t{}", entry);
+        }
     }
     Ok(())
 }
