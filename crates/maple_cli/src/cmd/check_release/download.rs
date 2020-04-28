293a8ee343ffe7bd843d662f153beb39cad74a29
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2020-04-28 11:34:09 +0800

    Refactor check_release

---
+++ crates/maple_cli/src/cmd/check_release/download.rs
@@ -0,0 +1,81 @@
+use super::{REPO, USER};
+use anyhow::Result;
+use std::fs::File;
+use std::io::copy;
+use std::path::{Path, PathBuf};
+
+fn get_asset_name() -> String {
+    let asset_name = if cfg!(target_os = "macos") {
+        "maple-x86_64-apple-darwin"
+    } else if cfg!(target_os = "linux") {
+        "maple-x86_64-unknown-linux-gnu"
+    } else if cfg!(target_os = "windows") {
+        "maple-x86_64-pc-windows-msvc"
+    } else {
+        "no-avaliable-prebuilt-binary"
+    };
+    asset_name.into()
+}
+
+pub fn to_download_url(version: &str) -> String {
+    format!(
+        "https://github.com/{}/{}/releases/download/{}/{}",
+        USER,
+        REPO,
+        version,
+        get_asset_name()
+    )
+}
+
+#[cfg(unix)]
+fn set_executable_permission<P: AsRef<Path>>(path: P) -> Result<()> {
+    use std::os::unix::fs::PermissionsExt;
+    let mut perms = std::fs::metadata(path.as_ref())?.permissions();
+    perms.set_mode(0o755);
+    std::fs::set_permissions(path.as_ref(), perms)?;
+    Ok(())
+}
+
+/// Downloads the latest remote release binary to a temp file.
+///
+/// # Arguments
+///
+/// - `version`: "v0.13"
+pub fn download_prebuilt_binary_to_a_tempfile(version: &str) -> Result<PathBuf> {
+    let target = to_download_url(version);
+
+    let mut response = reqwest::blocking::get(&target)?;
+
+    let (mut dest, temp_file) = {
+        let fname = response
+            .url()
+            .path_segments()
+            .and_then(|segments| segments.last())
+            .and_then(|name| if name.is_empty() { None } else { Some(name) })
+            .unwrap_or("tmp.bin");
+
+        let mut tmp_dir = std::env::temp_dir();
+        tmp_dir.push(format!("{}-{}", version, fname));
+        (File::create(&tmp_dir)?, tmp_dir)
+    };
+
+    copy(&mut response, &mut dest)?;
+
+    #[cfg(unix)]
+    set_executable_permission(&temp_file)?;
+
+    Ok(temp_file)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    #[ignore]
+    fn test_download_to_a_tempfile() {
+        let remote_release = latest_remote_release().unwrap();
+        let remote_tag = remote_release.tag_name;
+        download_prebuilt_binary_to_a_tempfile(&remote_tag).unwrap();
+    }
+}
--- crates/maple_cli/src/cmd/check_release/mod.rs
+++ crates/maple_cli/src/cmd/check_release/mod.rs
@@ -1,7 +1,8 @@
+mod download;
+
 use anyhow::{anyhow, Context, Result};
 use curl::easy::{Easy, List};
 use serde::{Deserialize, Serialize};
-use std::path::{Path, PathBuf};
 use structopt::StructOpt;
 
 const USER: &str = "liuchengxu";
@@ -35,7 +36,7 @@ impl CheckRelease {
                 println!(
                     "New maple release {} is avaliable, please download it from {} or rerun with --download flag.",
                     remote_tag,
-                    to_download_url(&remote_tag)
+                    download::to_download_url(&remote_tag)
                 );
             }
         } else {
@@ -54,7 +55,7 @@ impl CheckRelease {
                 "Current exe has to be under vim-clap/bin directory"
             ));
         }
-        let temp_file = download_prebuilt_binary_to_a_tempfile(version)?;
+        let temp_file = download::download_prebuilt_binary_to_a_tempfile(version)?;
         #[cfg(windows)]
         let bin_path = bin_dir.join("maple.exe");
         #[cfg(not(windows))]
@@ -115,72 +116,6 @@ fn extract_local_version_number(local_tag: &str) -> u32 {
     extract_remote_version_number(tag[0])
 }
 
-#[cfg(unix)]
-fn set_executable_permission<P: AsRef<Path>>(path: P) -> Result<()> {
-    use std::os::unix::fs::PermissionsExt;
-    let mut perms = std::fs::metadata(path.as_ref())?.permissions();
-    perms.set_mode(0o755);
-    std::fs::set_permissions(path.as_ref(), perms)?;
-    Ok(())
-}
-
-fn get_asset_name() -> String {
-    let asset_name = if cfg!(target_os = "macos") {
-        "maple-x86_64-apple-darwin"
-    } else if cfg!(target_os = "linux") {
-        "maple-x86_64-unknown-linux-gnu"
-    } else if cfg!(target_os = "windows") {
-        "maple-x86_64-pc-windows-msvc"
-    } else {
-        "no-avaliable-prebuilt-binary"
-    };
-    asset_name.into()
-}
-
-fn to_download_url(version: &str) -> String {
-    format!(
-        "https://github.com/{}/{}/releases/download/{}/{}",
-        USER,
-        REPO,
-        version,
-        get_asset_name()
-    )
-}
-
-/// Downloads the latest remote release binary to a temp file.
-///
-/// # Arguments
-///
-/// - `version`: "v0.13"
-fn download_prebuilt_binary_to_a_tempfile(version: &str) -> Result<PathBuf> {
-    use std::fs::File;
-    use std::io::copy;
-
-    let target = to_download_url(version);
-
-    let mut response = reqwest::blocking::get(&target)?;
-
-    let (mut dest, temp_file) = {
-        let fname = response
-            .url()
-            .path_segments()
-            .and_then(|segments| segments.last())
-            .and_then(|name| if name.is_empty() { None } else { Some(name) })
-            .unwrap_or("tmp.bin");
-
-        let mut tmp_dir = std::env::temp_dir();
-        tmp_dir.push(format!("{}-{}", version, fname));
-        (File::create(&tmp_dir)?, tmp_dir)
-    };
-
-    copy(&mut response, &mut dest)?;
-
-    #[cfg(unix)]
-    set_executable_permission(&temp_file)?;
-
-    Ok(temp_file)
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -191,13 +126,5 @@ mod tests {
         assert_eq!(13u32, extract_local_version_number(tag));
         let tag = "v0.13";
         assert_eq!(13u32, extract_local_version_number(tag));
-    }
-
-    #[test]
-    #[ignore]
-    fn test_download_to_a_tempfile() {
-        let remote_release = latest_remote_release().unwrap();
-        let remote_tag = remote_release.tag_name;
-        download_prebuilt_binary_to_a_tempfile(&remote_tag).unwrap();
     }
 }
