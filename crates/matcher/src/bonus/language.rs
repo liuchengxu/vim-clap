57411fcb186bb659124fdb219695b4a1e16f8ade
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2021-02-17 03:31:15 +0800

    Refactor calc_bonus_per_item()

--- crates/matcher/src/bonus/language.rs
+++ crates/matcher/src/bonus/language.rs
@@ -1,3 +1,7 @@
+//! Add a bonus to the comment line or the line that can have a declaration.
+//!
+//! Ref: https://github.com/jacktasia/dumb-jump/blob/master/dumb-jump.el
+
 use source_item::SourceItem;
 
 use crate::Score;
@@ -25,9 +29,25 @@ impl From<&str> for Language {
     }
 }
 
-// declaration
-// comment
-// https://github.com/jacktasia/dumb-jump/blob/master/dumb-jump.el
+/// Calculates the bonus by checking the first two items.
+fn calc_bonus_per_item(
+    trimmed_line: &str,
+    calc_bonus_fn: impl Fn(Option<&str>) -> Option<Score>,
+) -> Score {
+    let mut iter = trimmed_line.split_whitespace();
+
+    // Try the first two items because blines provider prepends the line number to the
+    // original line and the language bonus is mostly used in the blines provider.
+    let first_item = iter.next();
+
+    match calc_bonus_fn(first_item) {
+        Some(bonus) => bonus,
+        None => {
+            let second_item = iter.next();
+            calc_bonus_fn(second_item).unwrap_or_default()
+        }
+    }
+}
 
 impl Language {
     pub fn calc_bonus(&self, item: &SourceItem, base_score: Score) -> Score {
@@ -49,19 +69,7 @@ impl Language {
                     })
                 };
 
-                let mut iter = trimmed.split_whitespace();
-
-                // Try the first two items because blines provider prepends the line number to the
-                // original line and the language bonus is mostly used in the blines provider.
-                let first_item = iter.next();
-
-                match calc_bonus(first_item) {
-                    Some(bonus) => bonus,
-                    None => {
-                        let second_item = iter.next();
-                        calc_bonus(second_item).unwrap_or_default()
-                    }
-                }
+                calc_bonus_per_item(trimmed, calc_bonus)
             }
             "rs" => {
                 const TYPE: [&str; 3] = ["type", "mod", "impl"];
@@ -70,8 +78,7 @@ impl Language {
 
                 let calc_bonus = |item: Option<&str>| {
                     item.and_then(|s| {
-                        // FIXME find a proper strategy.
-                        if s.starts_with("pub") || s.starts_with("[cfg(feature") {
+                        if s.starts_with("pub") {
                             Some(base_score / 6)
                         } else if TYPE.contains(&s) {
                             Some(base_score / 5)
@@ -79,6 +86,8 @@ impl Language {
                             Some(base_score / 4)
                         } else if VARIABLE.contains(&s) {
                             Some(base_score / 3)
+                        } else if s.starts_with("[cfg(feature") {
+                            Some(base_score / 7)
                         } else if s.starts_with("//") {
                             Some(-(base_score / 5))
                         } else {
@@ -87,11 +96,7 @@ impl Language {
                     })
                 };
 
-                let mut iter = trimmed.split_whitespace();
-                match calc_bonus(iter.next()) {
-                    Some(bonus) => bonus,
-                    None => calc_bonus(iter.next()).unwrap_or_default(),
-                }
+                calc_bonus_per_item(trimmed, calc_bonus)
             }
 
             _ => 0,
