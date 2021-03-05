0ca95d37c0a3608e689f2b1b76bbae6a3db6e801
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2021-03-06 00:41:10 +0800

    Support unordered substring query
    Close #651

--- crates/matcher/src/algo.rs
+++ crates/matcher/src/algo.rs
@@ -58,7 +58,44 @@ pub mod substring {
         slice[start_at..].find(pat).map(|i| start_at + i)
     }
 
-    fn substr_indices_impl(haystack: &str, niddle: &str) -> Option<(f64, Vec<usize>)> {
+    fn _substr_indices_impl(haystack: &str, niddle: &str) -> Option<(f64, Vec<usize>)> {
+        let niddle = niddle.to_lowercase();
+
+        match find_start_at(haystack, 0, &niddle) {
+            Some(idx) => {
+                let mut positions = Vec::new();
+
+                // For build without overflow checks this could be written as
+                // `let mut pos = idx - 1;` with `|| { pos += 1; pos }` closure.
+                let mut pos = idx;
+                positions.resize_with(
+                    niddle.len(),
+                    // Simple endless iterator for `idx..` range. Even though it's endless,
+                    // it will iterate only `sub_niddle.len()` times.
+                    || {
+                        pos += 1;
+                        pos - 1
+                    },
+                );
+
+                if positions.is_empty() {
+                    return None;
+                }
+
+                let calc_score = || {
+                    let last_pos = positions.last().unwrap();
+                    let match_len = (last_pos + 1 - positions[0]) as f64;
+
+                    (2f64 / (positions[0] + 1) as f64) + 1f64 / (last_pos + 1) as f64 - match_len
+                };
+
+                Some((calc_score(), positions))
+            }
+            None => None,
+        }
+    }
+
+    fn unordered_substr_indices_impl(haystack: &str, niddle: &str) -> Option<(f64, Vec<usize>)> {
         // unreasonably large haystack
         if haystack.len() > 1024 {
             return None;
@@ -67,28 +104,14 @@ pub mod substring {
         let haystack = haystack.to_lowercase();
         let haystack = haystack.as_str();
 
-        let mut offset = 0;
+        let mut total_score = 0f64;
         let mut positions = Vec::new();
         for sub_niddle in niddle.split_whitespace() {
-            let sub_niddle = sub_niddle.to_lowercase();
-
-            match find_start_at(haystack, offset, &sub_niddle) {
-                Some(idx) => {
-                    offset = idx + sub_niddle.len();
-                    // For build without overflow checks this could be written as
-                    // `let mut pos = idx - 1;` with `|| { pos += 1; pos }` closure.
-                    let mut pos = idx;
-                    positions.resize_with(
-                        positions.len() + sub_niddle.len(),
-                        // Simple endless iterator for `idx..` range. Even though it's endless,
-                        // it will iterate only `sub_niddle.len()` times.
-                        || {
-                            pos += 1;
-                            pos - 1
-                        },
-                    );
-                }
-                None => return None,
+            if let Some((score, indices)) = _substr_indices_impl(haystack, &sub_niddle) {
+                total_score += score;
+                positions.extend_from_slice(&indices);
+            } else {
+                return None;
             }
         }
 
@@ -96,24 +119,26 @@ pub mod substring {
             return Some((0f64, positions));
         }
 
-        let last_pos = positions.last().unwrap();
-        let match_len = (last_pos + 1 - positions[0]) as f64;
+        positions.sort_unstable();
 
-        Some((
-            (2f64 / (positions[0] + 1) as f64) + 1f64 / (last_pos + 1) as f64 - match_len,
-            positions,
-        ))
+        Some((total_score, positions))
     }
 
     pub fn substr_indices(haystack: &str, niddle: &str) -> Option<(i64, Vec<usize>)> {
-        substr_indices_impl(haystack, niddle).map(|(score, positions)| (score as i64, positions))
+        unordered_substr_indices_impl(haystack, niddle)
+            .map(|(score, positions)| (score as i64, positions))
     }
 
     #[test]
     fn test_substr() {
         assert_eq!(
             substr_indices("src/bun/blune", "sr bl"),
-            Some((-7, vec![0, 1, 8, 9]))
+            Some((-1, vec![0, 1, 8, 9]))
+        );
+
+        assert_eq!(
+            substr_indices("src/bun/blune", "bl sr"),
+            Some((-1, vec![0, 1, 8, 9]))
         );
     }
 }
