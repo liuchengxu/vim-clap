e195ec43727491b1bfee48a4fe7dd62bc6ff54ff
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2021-08-14 20:18:29 +0800

    .

--- crates/types/src/lib.rs
+++ crates/types/src/lib.rs
@@ -3,5 +3,8 @@ mod search_term;
 mod source_item;
 
 pub use self::query::Query;
-pub use self::search_term::{ExactTermType, FuzzyTermType, InverseTermType, SearchTerm, TermType};
+pub use self::search_term::{
+    ExactTerm, ExactTermType, FuzzyTerm, FuzzyTermType, InverseTerm, InverseTermType, SearchTerm,
+    TermType,
+};
 pub use self::source_item::{FilteredItem, MatchText, MatchTextFor, MatchType, SourceItem};
