8dacd2c817b990f6b5b43746d88b8ea4927d5152
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2021-08-23 23:39:24 +0800

    Add cwd.rs

---
+++ crates/matcher/src/bonus/cwd.rs
@@ -0,0 +1,35 @@
+//! Add a bonus score for the file matching current working directory.
+
+use types::SourceItem;
+
+use crate::Score;
+
+/// Used for recent_files provider.
+///
+/// Each entry of recent_files provider is an absolute path String.
+#[derive(Clone, Debug)]
+pub struct Cwd {
+    /// Absolute path String.
+    pub abs_path: String,
+}
+
+impl From<String> for Cwd {
+    fn from(abs_path: String) -> Self {
+        Self { abs_path }
+    }
+}
+
+impl Cwd {
+    pub fn calc_bonus(&self, item: &SourceItem, base_score: Score) -> Score {
+        log::debug!(
+            "---- matching item: {:?}, abs_path: {:?}",
+            item,
+            self.abs_path
+        );
+        if item.raw.starts_with(&self.abs_path) {
+            base_score / 2
+        } else {
+            0
+        }
+    }
+}
