91320f89cad2fba494885bcb6d1195b8f54878e1
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2021-09-07 22:52:42 +0800

    .

---
+++ crates/maple_cli/src/command/grep/forerunner.rs
@@ -0,0 +1,87 @@
+use super::*;
+
+#[derive(StructOpt, Debug, Clone)]
+pub struct RipGrepForerunner {
+    /// Specify the working directory of CMD
+    #[structopt(long = "cmd-dir", parse(from_os_str))]
+    cmd_dir: Option<PathBuf>,
+
+    /// Specify the threshold for writing the output of command to a tempfile.
+    #[structopt(long = "output-threshold", default_value = "30000")]
+    output_threshold: usize,
+}
+
+impl RipGrepForerunner {
+    /// Skip the forerunner job if `cmd_dir` is not a git repo.
+    ///
+    /// Only spawn the forerunner job for git repo for now.
+    fn should_skip(&self) -> bool {
+        if let Some(ref dir) = self.cmd_dir {
+            if !is_git_repo(dir) {
+                return true;
+            }
+        } else if let Ok(dir) = std::env::current_dir() {
+            if !is_git_repo(&dir) {
+                return true;
+            }
+        }
+        false
+    }
+
+    pub fn run(
+        self,
+        Params {
+            number,
+            icon_painter,
+            no_cache,
+            ..
+        }: Params,
+    ) -> Result<()> {
+        if !no_cache {
+            if let Some(ref dir) = self.cmd_dir {
+                let base_cmd = BaseCommand::new(RG_EXEC_CMD.into(), dir.clone());
+                if let Some((total, cache)) = base_cmd.cache_info() {
+                    send_response_from_cache(
+                        &cache,
+                        total as usize,
+                        SendResponse::Json,
+                        Some(IconPainter::Grep),
+                    );
+                    return Ok(());
+                }
+            }
+        }
+
+        if self.should_skip() {
+            return Ok(());
+        }
+
+        let mut std_cmd = StdCommand::new(RG_ARGS[0]);
+        // Do not use --vimgrep here.
+        std_cmd.args(&RG_ARGS[1..]);
+
+        if let Some(ref dir) = self.cmd_dir {
+            std_cmd.current_dir(dir);
+        }
+
+        let mut cmd = std_cmd.into_inner();
+
+        let mut light_cmd = LightCommand::new_grep(
+            &mut cmd,
+            self.cmd_dir.clone(),
+            number,
+            icon_painter,
+            Some(self.output_threshold),
+        );
+
+        let cwd = match self.cmd_dir {
+            Some(d) => d,
+            None => std::env::current_dir()?,
+        };
+        let base_cmd = BaseCommand::new(RG_EXEC_CMD.into(), cwd);
+
+        light_cmd.execute(base_cmd)?.print();
+
+        Ok(())
+    }
+}
--- crates/maple_cli/src/command/grep/mod.rs
+++ crates/maple_cli/src/command/grep/mod.rs
@@ -1,3 +1,7 @@
+mod forerunner;
+
+pub use self::forerunner::RipGrepForerunner;
+
 use std::convert::TryFrom;
 use std::path::{Path, PathBuf};
 use std::process::Command;
@@ -218,17 +222,6 @@ impl Grep {
     }
 }
 
-#[derive(StructOpt, Debug, Clone)]
-pub struct RipGrepForerunner {
-    /// Specify the working directory of CMD
-    #[structopt(long = "cmd-dir", parse(from_os_str))]
-    cmd_dir: Option<PathBuf>,
-
-    /// Specify the threshold for writing the output of command to a tempfile.
-    #[structopt(long = "output-threshold", default_value = "30000")]
-    output_threshold: usize,
-}
-
 #[derive(Debug, Clone)]
 pub struct RgBaseCommand {
     pub inner: BaseCommand,
@@ -242,81 +235,6 @@ impl RgBaseCommand {
 
     pub fn cache_info(&self) -> Option<(usize, PathBuf)> {
         self.inner.cache_info()
-    }
-}
-
-impl RipGrepForerunner {
-    /// Skip the forerunner job if `cmd_dir` is not a git repo.
-    ///
-    /// Only spawn the forerunner job for git repo for now.
-    fn should_skip(&self) -> bool {
-        if let Some(ref dir) = self.cmd_dir {
-            if !is_git_repo(dir) {
-                return true;
-            }
-        } else if let Ok(dir) = std::env::current_dir() {
-            if !is_git_repo(&dir) {
-                return true;
-            }
-        }
-        false
-    }
-
-    pub fn run(
-        self,
-        Params {
-            number,
-            icon_painter,
-            no_cache,
-            ..
-        }: Params,
-    ) -> Result<()> {
-        if !no_cache {
-            if let Some(ref dir) = self.cmd_dir {
-                let base_cmd = BaseCommand::new(RG_EXEC_CMD.into(), dir.clone());
-                if let Some((total, cache)) = base_cmd.cache_info() {
-                    send_response_from_cache(
-                        &cache,
-                        total as usize,
-                        SendResponse::Json,
-                        Some(IconPainter::Grep),
-                    );
-                    return Ok(());
-                }
-            }
-        }
-
-        if self.should_skip() {
-            return Ok(());
-        }
-
-        let mut std_cmd = StdCommand::new(RG_ARGS[0]);
-        // Do not use --vimgrep here.
-        std_cmd.args(&RG_ARGS[1..]);
-
-        if let Some(ref dir) = self.cmd_dir {
-            std_cmd.current_dir(dir);
-        }
-
-        let mut cmd = std_cmd.into_inner();
-
-        let mut light_cmd = LightCommand::new_grep(
-            &mut cmd,
-            self.cmd_dir.clone(),
-            number,
-            icon_painter,
-            Some(self.output_threshold),
-        );
-
-        let cwd = match self.cmd_dir {
-            Some(d) => d,
-            None => std::env::current_dir()?,
-        };
-        let base_cmd = BaseCommand::new(RG_EXEC_CMD.into(), cwd);
-
-        light_cmd.execute(base_cmd)?.print();
-
-        Ok(())
     }
 }
 
