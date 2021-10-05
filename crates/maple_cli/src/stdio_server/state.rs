163854635f4cf1d136715776f620e5069463e7e8
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2021-10-06 07:13:49 +0800

    Commit state.rs/vim.rs

---
+++ crates/maple_cli/src/stdio_server/state.rs
@@ -0,0 +1,31 @@
+use std::sync::Arc;
+
+use crossbeam_channel::Sender;
+use serde::Serialize;
+
+use crate::stdio_server::rpc_client::RpcClient;
+use crate::stdio_server::types::Call;
+use crate::stdio_server::vim::Vim;
+
+/// Current State of Vim/NeoVim client.
+#[derive(Serialize)]
+pub struct State {
+    #[serde(skip_serializing)]
+    pub tx: Sender<Call>,
+
+    #[serde(skip_serializing)]
+    pub vim: Vim,
+
+    /// Highlight match ids.
+    pub highlights: Vec<u32>,
+}
+
+impl State {
+    pub fn new(tx: Sender<Call>, client: Arc<RpcClient>) -> Self {
+        Self {
+            tx,
+            vim: Vim::new(client),
+            highlights: Default::default(),
+        }
+    }
+}
