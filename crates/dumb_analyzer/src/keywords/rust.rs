9d01d0c301ae08d23396908ce1db688abff20991
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2022-02-26 22:31:12 +0800

    .

--- crates/dumb_analyzer/src/keywords/rust.rs
+++ crates/dumb_analyzer/src/keywords/rust.rs
@@ -1,17 +1,25 @@
-const TYPE: &[&str] = &["type", "mod", "impl"];
-const FUNCTION: &[&str] = &["fn", "macro_rules"];
-const VARIABLE: &[&str] = &["let", "const", "static", "enum", "struct", "trait"];
+const DEFINITION: &[&str] = &[
+    "enum", "trait", "struct", "fn", "const", "static", "crate", "mod",
+];
 
-pub fn pattern_weight(token: &str) -> Option<usize> {
-    if FUNCTION.contains(&token) {
+const REFERENCE: &[&str] = &["use", "impl", "let"];
+
+const STATEMENT: &[&str] = &[
+    "as", "break", "continue", "else", "extern", "false", "for", "if", "impl", "in", "let", "loop",
+    "match", "move", "mut", "pub", "ref", "return", "self", "Self", "static", "super", "true",
+    "type", "unsafe", "where", "while",
+];
+
+pub fn pattern_weighttoken(token: &str) -> Option<usize> {
+    if DEFINITION.contains(&token) {
         Some(4)
-    } else if token.starts_with("pub") {
+    } else if REFERENCE.contains(&token) {
         Some(6)
-    } else if TYPE.contains(&token) {
+    } else if token.starts_with("pub") {
         Some(7)
-    } else if VARIABLE.contains(&token) {
+    } else if STATEMENT.contains(&token) {
         Some(8)
-    } else if token.starts_with("[cfg(feature") {
+    } else if token.starts_with("[cfg") {
         Some(10)
     } else {
         None
