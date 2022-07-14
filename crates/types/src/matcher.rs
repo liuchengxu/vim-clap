20f84ae727b9289074780066ac693c8a0153bb75
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2022-07-14 18:32:07 +0800

    Commit types/src/matcher.rs

---
+++ crates/types/src/matcher.rs
@@ -0,0 +1,15 @@
+/// Score of base matching algorithm(fzy, skim, etc).
+pub type Score = i64;
+
+/// A tuple of (score, matched_indices) for the line has a match given the query string.
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct MatchResult {
+    pub score: Score,
+    pub indices: Vec<usize>,
+}
+
+impl MatchResult {
+    pub fn new(score: Score, indices: Vec<usize>) -> Self {
+        Self { score, indices }
+    }
+}
