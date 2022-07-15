3bbe75be459e871fcd550fe381507ed42f0dde99
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2022-07-15 18:48:47 +0800

    Ignore ctags null tags warning messages

--- crates/maple_cli/src/find_usages/search_engine/ctags/mod.rs
+++ crates/maple_cli/src/find_usages/search_engine/ctags/mod.rs
@@ -67,7 +67,7 @@ impl<'a, P: AsRef<Path> + Hash> CtagsSearcher<'a, P> {
     fn build_exec(&self, query: &str, query_type: QueryType) -> Exec {
         // https://docs.ctags.io/en/latest/man/readtags.1.html#examples
         let cmd = Exec::cmd("readtags")
-            .stderr(Redirection::Merge)
+            .stderr(Redirection::None) // Ignore the line: ctags: warning...
             .arg("--tag-file")
             .arg(&self.tags_path)
             .arg("-E")
