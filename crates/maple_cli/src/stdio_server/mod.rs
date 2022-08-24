78cbab2eb908039b2fbd40e1fcd047b6dee89741
Liu-Cheng Xu <xuliuchengxlc@gmail.com>
2022-08-25 01:32:15 +0800

    Merge session_client.rs

--- crates/maple_cli/src/stdio_server/mod.rs
+++ crates/maple_cli/src/stdio_server/mod.rs
@@ -1,7 +1,6 @@
 mod impls;
 mod rpc;
 mod session;
-mod session_client;
 mod state;
 mod types;
 mod vim;
@@ -10,14 +9,21 @@ use std::io::{BufReader, BufWriter};
 use std::ops::Deref;
 use std::sync::Arc;
 
+use anyhow::Result;
 use once_cell::sync::OnceCell;
+use parking_lot::Mutex;
+use serde_json::{json, Value};
+use tokio::sync::mpsc::UnboundedReceiver;
 
-use self::rpc::RpcClient;
-use self::session_client::SessionClient;
+use self::impls::dumb_jump::DumbJumpProvider;
+use self::impls::filer::FilerProvider;
+use self::impls::recent_files::RecentFilesProvider;
+use self::impls::DefaultProvider;
+use self::rpc::{Call, MethodCall, Notification, RpcClient};
+use self::session::{ClapProvider, ProviderEvent, SessionContext, SessionManager};
 use self::state::State;
 use self::types::GlobalEnv;
-
-pub use self::rpc::{MethodCall, Notification};
+use self::vim::Vim;
 
 static GLOBAL_ENV: OnceCell<GlobalEnv> = OnceCell::new();
 
@@ -45,4 +51,153 @@ pub async fn start() {
     let state = State::new(call_tx, rpc_client.clone());
     let session_client = SessionClient::new(state, rpc_client);
     session_client.loop_call(call_rx).await;
+}
+
+#[derive(Clone)]
+struct SessionClient {
+    vim: Vim,
+    pub state_mutex: Arc<Mutex<State>>,
+    pub session_manager_mutex: Arc<Mutex<SessionManager>>,
+}
+
+impl SessionClient {
+    /// Creates a new instnace of [`SessionClient`].
+    fn new(state: State, rpc_client: Arc<RpcClient>) -> Self {
+        let vim = Vim::new(rpc_client);
+        Self {
+            vim,
+            state_mutex: Arc::new(Mutex::new(state)),
+            session_manager_mutex: Arc::new(Mutex::new(SessionManager::default())),
+        }
+    }
+
+    /// Entry of the bridge between Vim and Rust.
+    ///
+    /// Handle the message actively initiated from Vim.
+    async fn loop_call(self, mut rx: UnboundedReceiver<Call>) {
+        while let Some(call) = rx.recv().await {
+            let session_client = self.clone();
+            tokio::spawn(async move {
+                match call {
+                    Call::Notification(notification) => {
+                        if let Err(e) = session_client.process_notification(notification).await {
+                            tracing::error!(error = ?e, "Error at handling a Vim Notification");
+                        }
+                    }
+                    Call::MethodCall(method_call) => {
+                        let id = method_call.id;
+
+                        match session_client.process_method_call(method_call).await {
+                            Ok(Some(result)) => {
+                                // Send back the result of method call.
+                                let state = session_client.state_mutex.lock();
+                                if let Err(e) = state.vim.send(id, Ok(result)) {
+                                    tracing::debug!(error = ?e, "Failed to send the output result");
+                                }
+                            }
+                            Ok(None) => {}
+                            Err(e) => {
+                                tracing::error!(error = ?e, "Error at handling a Vim MethodCall");
+                            }
+                        }
+                    }
+                }
+            });
+        }
+    }
+
+    /// Process the notification message from Vim.
+    async fn process_notification(&self, notification: Notification) -> Result<()> {
+        use ProviderEvent::*;
+
+        match notification.method.as_str() {
+            "initialize_global_env" => notification.initialize_global_env(), // should be called only once.
+
+            "note_recent_files" => notification.note_recent_file().await,
+
+            "on_init" | "dumb_jump/on_init" | "recent_files/on_init" | "filer/on_init" => {
+                let call = Call::Notification(notification);
+                let context: SessionContext = call.clone().into();
+
+                tracing::debug!("======================== New context {context:?}");
+                let provider_id = self.vim.current_provider_id().await?;
+                tracing::debug!("======================== New provider id {provider_id:?}");
+                let provider: Box<dyn ClapProvider> = match provider_id.as_str() {
+                    "dumb_jump" => Box::new(DumbJumpProvider::new(self.vim.clone(), context)),
+                    "recent_files" => Box::new(RecentFilesProvider::new(self.vim.clone(), context)),
+                    "filer" => Box::new(FilerProvider::new(self.vim.clone(), context)),
+                    _ => Box::new(DefaultProvider::new(self.vim.clone(), context)),
+                };
+
+                tracing::debug!("======================== Try locking");
+                let session_manager = self.session_manager_mutex.clone();
+                let mut session_manager = session_manager.lock();
+                tracing::debug!("======================== New session");
+                session_manager.new_session(call, provider);
+
+                Ok(())
+            }
+
+            "on_typed" | "filer/on_typed" | "dumb_jump/on_typed" | "recent_files/on_typed" => {
+                let session_manager = self.session_manager_mutex.lock();
+                session_manager.send(
+                    notification.session_id.ok_or_else(|| {
+                        anyhow::anyhow!("Each provider notification must contain a session id")
+                    })?,
+                    OnTyped,
+                );
+                Ok(())
+            }
+
+            "on_move" | "filer/on_move" | "dumb_jump/on_move" | "recent_files/on_move" => {
+                let session_manager = self.session_manager_mutex.lock();
+                session_manager.send(
+                    notification.session_id.ok_or_else(|| {
+                        anyhow::anyhow!("Each provider notification must contain a session id")
+                    })?,
+                    OnMove,
+                );
+                Ok(())
+            }
+
+            "exit" => {
+                let mut session_manager = self.session_manager_mutex.lock();
+                session_manager.terminate(
+                    notification
+                        .session_id
+                        .expect("SessionId should be included in exit message"),
+                );
+                Ok(())
+            }
+            _ => Err(anyhow::anyhow!("Unknown notification: {notification:?}")),
+        }
+    }
+
+    /// Process the method call message from Vim.
+    async fn process_method_call(&self, method_call: MethodCall) -> Result<Option<Value>> {
+        let msg = method_call;
+
+        if msg.method != "init_ext_map" {
+            tracing::debug!(?msg, "==> stdio message(in)");
+        }
+
+        let value = match msg.method.as_str() {
+            "init_ext_map" => Some(msg.parse_filetypedetect()),
+            "preview/file" => Some(msg.preview_file().await?),
+            "quickfix" => Some(msg.preview_quickfix().await?),
+
+            // Deprecated but not remove them for now.
+            "on_move" => {
+                let session_manager = self.session_manager_mutex.lock();
+                session_manager.send(msg.session_id, ProviderEvent::OnMove);
+                None
+            }
+
+            _ => Some(json!({
+                "error": format!("Unknown method call: {}", msg.method)
+            })),
+        };
+
+        Ok(value)
+    }
 }
